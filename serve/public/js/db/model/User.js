0639e645051da3970c45b8d3481fa4aa407b57fd
whistleStudio <435424527@qq.com>
2022-03-29 17:56:49 +0800

    202203291756

--- serve/public/js/db/model/User.js
+++ serve/public/js/db/model/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 var hash = require('object-hash')
 
+//role 0-生成用户 1-普通用户 2-老师 3-普通管理员
 const userSchema = new mongoose.Schema({
   name: String,
   pwd: String,
@@ -10,10 +11,12 @@ const userSchema = new mongoose.Schema({
   authority: {type: Number, default: 1},
   authDate: {type: Date, default: new Date()},
   tel: Number,
-  role: Number,
+  role: {type: Number, default: 1},
   score: Number,
   code: {type: String, default: genCode()},
-  avatar: {type: Number, default: 0}
+  avatar: {type: Number, default: 0},
+  school: String,
+  pmail: String
 })
 
 function genCode () {
